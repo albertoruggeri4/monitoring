7c6c0c66f55ee0d98cd28c35e931ff00a67e511a
albertoruggeri4 <62298067+albertoruggeri4@users.noreply.github.com>
2020-05-20 17:38:35 +0200

    Update R.code_faPAR.r

--- R.code_faPAR.r
+++ R.code_faPAR.r
@@ -59,3 +59,76 @@ faPAR10 <- raster("faPAR10.tif")
 
 levelplot(faPAR10) 
 
+##### regression model between faPAR and NDVI
+
+#correlation in linear model
+#PAR and NDVI
+erosion <- c(12, 14, 16, 24, 26, 40, 55, 67)
+hm <- c(30, 100, 150, 200, 260, 340, 460, 600)
+
+plot(erosion, hm, col='red', pch=19, xlab='erosion',ylab="heavy metals", cex=2)
+
+#function lm
+model1 <- lm(hm ~ erosion)
+
+#for know the information
+summary(model1)
+
+#coefficient of .. R-square...  ~1 --> coorelation 0 --> no correlation
+#p-value = ??
+#y=b=bx+a   a=intercept b= 
+#the function is abline
+abline(model1)
+
+#the same with faPAR and NDVI
+
+library(raster)
+library(rasterdiv) #for plot copNDVI
+
+setwd("C:/Lab")
+faPAR10 <- raster("faPAR10.tif")
+plot(faPAR10)
+plot(copNDVI)
+
+#remove value correlated to water
+copNDVI <- reclassify(copNDVI, cbind(253:255, NA), right=TRUE)
+
+#let see how many cell there is in the raster, put the name
+faPAR10 #check ncell
+
+#with the library sf and the function of random.points we canr reduce the ncell
+library(sf) # to call st_* functions
+
+
+#the n is the number of the points / x = the argumetn
+random.points <- function(x,n)
+{
+lin <- rasterToContour(is.na(x))
+pol <- as(st_union(st_polygonize(st_as_sf(lin))), 'Spatial') # st_union to dissolve geometries
+pts <- spsample(pol[1,], n, type = 'random')
+}
+
+#example, selecting from faPAR10 1000 random points
+pts <- random.points(faPAR10,1000)
+
+#extract is the function to extract the point, argument,
+
+copNDVIp <- extract(copNDVI, pts)
+
+#the same for faPAR
+
+faPAR10p <- extract(faPAR10,pts)
+
+#if you put the name of the file you can see the points, NA is same missmatching
+
+
+#photosynthesis vs. biomss
+model2 <- lm(faPAR1P ~ copNDVIp)
+summary(model2)
+#you can see that R square is about 0.4 == there isn't correlation 
+
+plot(copNDVIp, faPAR10p, col='green', xlab='biomass', ylab='photosynthesis')
+abline(model2, col='red')
+
+#there are a lot of point far from the line ,because there isn't correlation about photosynthesis and biomass
+#check with levelplot (copNDVI and faPAR10 ..library(rastervis) taìhat we did the last time,  for explain this uncorrelation
