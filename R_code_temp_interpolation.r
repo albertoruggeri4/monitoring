673e258b987cecbf682351fbb3510653968ed8cd
albertoruggeri4 <62298067+albertoruggeri4@users.noreply.github.com>
2020-06-08 16:56:52 +0200

    Create R_code_temp_interpolation.r

---
+++ R_code_temp_interpolation.r
@@ -0,0 +1,4 @@
+#### data of measurement of different chimical compost of soil and different vegetation of the same soil
+
+#Spatial statistics
+#Interpolation only spatial and of the species distribution modelling
